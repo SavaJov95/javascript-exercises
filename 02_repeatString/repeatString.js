8b8840561a25bcda5e9c802159e8d5f43bc91284
SavaJ95 <savajovanovic1995@gmail.com>
2022-10-30 01:50:12 +0200

    asd

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+let rec = "hey";
 
+const repeatString = function(string, num) {
+    for(i=1; i<num; i++) {
+        rec += string;
+    }
+		return rec;
 };
+
+repeatString(rec, 3);
 
 // Do not edit below this line
 module.exports = repeatString;
